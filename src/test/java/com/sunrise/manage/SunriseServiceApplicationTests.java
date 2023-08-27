76e4dd447aacd18346656c863750d9846ff016b0
sakunthalayerra <203j5a0519@raghuinstech.com>
2023-08-27 12:47:39 +0530

    intial source code

---
+++ src/test/java/com/sunrise/manage/SunriseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunrise.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
